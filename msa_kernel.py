c06e77b3ed5306f1c9f111284be7009d3f7d1827
alexzhang13 <alex.lx.zhang@gmail.com>
2024-09-14 20:56:37 -0700

    Added online softmax to scale number of residuals!

--- msa_kernel.py
+++ msa_kernel.py
@@ -28,6 +28,7 @@ def MSAFwdFused(
     RES_LEN_POW2: tl.constexpr,
     SEQ_LEN: tl.constexpr, RES_LEN: tl.constexpr, 
     BLOCK_SIZE_ROW: tl.constexpr,
+    BLOCK_SIZE_COL: tl.constexpr,
     BLOCK_SIZE_SEQ: tl.constexpr,
 ):
     # Compute the program ID and starting index
@@ -39,32 +40,73 @@ def MSAFwdFused(
     h_off = pid_h.to(tl.int64)
     i_off = pid_i.to(tl.int64) * BLOCK_SIZE_ROW
     offs_i = i_off + tl.arange(0, BLOCK_SIZE_ROW)
-    offs_j = tl.arange(0, RES_LEN_POW2)
     offs_c = tl.arange(0, C_LEN_POW2)
     
-    # Load in b weight i:i+BLOCK_SIZE_ROW and compute softmax
-    b_offs = (z_off * RES_LEN * RES_LEN * N_head) + \
-            (offs_i[:, None] * RES_LEN * N_head) + \
-            (offs_j[None, :] * N_head) + \
-            (h_off)
+    # Use exp2 for Triton
+    log2_e = 1.44269504089
+
+    prev_row_max = tl.full((BLOCK_SIZE_ROW, 1), 0.0, dtype=tl.float32)
+    new_row_max = tl.full((BLOCK_SIZE_ROW, 1), 0.0, dtype=tl.float32)
+    l = tl.full((BLOCK_SIZE_ROW, 1), 0.0, dtype=tl.float32)
     
-    ij_mask = ((offs_i < RES_LEN)[:, None]) & ((offs_j < RES_LEN)[None, :])
-    
-    b = tl.load(b_ij_ptr + b_offs, ij_mask, -inf)
-    
-    # Compute softmax of row (assuming all loaded in, should be cheap since w is small)
-    # log2_e = 1.44269504
-    # exp2 = lambda x : tl.exp2(log2_e * x)
-    
-    row_minus_max = b - tl.max(b, axis=1, keep_dims=True)
-    numerator = tl.exp(row_minus_max)
-    denominator = tl.sum(numerator, axis=1, keep_dims=True)
-    w = numerator / denominator
-    
-    # Compute output
-    for s in range(0, SEQ_LEN, BLOCK_SIZE_SEQ):
-        # Offsets for {s,i} indices
+    for j in range(0, RES_LEN, BLOCK_SIZE_COL):
+        # Load in b weight i:i+BLOCK_SIZE_ROW and compute softmax
+        offs_j = j + tl.arange(0, BLOCK_SIZE_COL)
+        b_offs = (z_off * RES_LEN * RES_LEN * N_head) + \
+                (offs_i[:, None] * RES_LEN * N_head) + \
+                (offs_j[None, :] * N_head) + \
+                (h_off)
+
+        ij_mask = ((offs_i < RES_LEN)[:, None]) & ((offs_j < RES_LEN)[None, :])
         
+        # Load current b's
+        b = tl.load(b_ij_ptr + b_offs, ij_mask, -inf)
+        
+        # Compute softmax statistics (to broadcast)
+        new_row_max = tl.maximum(tl.max(b, axis=1, keep_dims=True), prev_row_max)
+        
+        w = tl.exp2(log2_e * (b - new_row_max))
+        l *= tl.exp2(log2_e * (prev_row_max - new_row_max))
+        l += tl.sum(w, axis=1, keep_dims=True)
+        
+        # w = tl.exp(b)
+        # l += tl.sum(w, axis=1, keep_dims=True)
+        # tl.device_print(tl.sum(w, axis=1, keep_dims=True).shape)
+    
+        # Compute vw portion
+        for s in range(0, SEQ_LEN, BLOCK_SIZE_SEQ):
+            # Offsets for {s,i} indices
+            for ch in range(0, C_hidden, 1):
+                offs_s = s + tl.arange(0, BLOCK_SIZE_SEQ)
+                si_off = (z_off * SEQ_LEN * RES_LEN * N_head * C_hidden) + \
+                     (offs_s[None, :] * RES_LEN * N_head * C_hidden) + \
+                     (offs_i[:, None] * N_head * C_hidden) + \
+                     (h_off * C_hidden) + \
+                     (ch)
+                sj_off = (z_off * SEQ_LEN * RES_LEN * N_head * C_hidden) + \
+                         (offs_s[None, :] * RES_LEN * N_head * C_hidden) + \
+                         (offs_j[:, None] * N_head * C_hidden) + \
+                         (h_off * C_hidden) + \
+                         (ch)
+                si_mask = ((offs_s < SEQ_LEN)[None, :]) & ((offs_i < RES_LEN)[:, None])
+                sj_mask = ((offs_s < SEQ_LEN)[None, :]) & ((offs_j < RES_LEN)[:, None])
+
+                # Load in v_{s,j} transposed
+                v = tl.load(v_si_ptr + sj_off, sj_mask, 0)
+                
+                # Pay with extra loads of the outputs
+                vw = tl.load(output_ptr + si_off, si_mask, 0)
+                vw = vw * (tl.exp2(log2_e * (prev_row_max - new_row_max)))
+                
+                # (I x J) x (J x S) = I x S
+                vw = tl.dot(w, v, acc=vw)
+                
+                # Store vw in output 
+                tl.store(output_ptr + si_off, vw, si_mask)
+        prev_row_max = new_row_max
+        
+    # Compute outputs after vw softmax is handled
+    for s in range(0, SEQ_LEN, BLOCK_SIZE_SEQ):
         for ch in range(0, C_hidden, 1):
             offs_s = s + tl.arange(0, BLOCK_SIZE_SEQ)
             si_off = (z_off * SEQ_LEN * RES_LEN * N_head * C_hidden) + \
@@ -78,22 +120,10 @@ def MSAFwdFused(
             g = tl.load(g_si_ptr + si_off, si_mask, 0)
             g = tl.sigmoid(g)
 
-            sj_off = (z_off * SEQ_LEN * RES_LEN * N_head * C_hidden) + \
-                     (offs_s[None, :] * RES_LEN * N_head * C_hidden) + \
-                     (offs_j[:, None] * N_head * C_hidden) + \
-                     (h_off * C_hidden) + \
-                     (ch)
-            sj_mask = ((offs_s < SEQ_LEN)[None, :]) & ((offs_j < RES_LEN)[:, None])
-
-            # Load in v_{s,j} transposed
-            v = tl.load(v_si_ptr + sj_off, sj_mask, 0)
+            # vw is currently in out in memory.
+            vw = tl.load(output_ptr + si_off, si_mask, 0)
+            vw = vw / l
             
-            # W has shape I x J
-            vw = tl.zeros_like(g)
-            
-            vw = tl.dot(w, v, vw) # (I x J) x (J x S) = I x S
-
-            # Element-wise product of output
             out = g * vw
             tl.store(output_ptr + si_off, out, si_mask)
 
@@ -109,7 +139,8 @@ class _MSAWeightedAveragingFused(torch.autograd.Function):
         # allocate output
         out = torch.empty((n_batches, n_seq, n_res, no_heads * C_hidden), device=g.device, dtype=g.dtype)
 
-        BLOCK_SIZE_ROW = 16
+        BLOCK_SIZE_ROW = 32
+        BLOCK_SIZE_COL = 16
         BLOCK_SIZE_SEQ = 16
         n_res_pow2 = nearest_pow2(n_res)
         c_hidden_pow2 = nearest_pow2(C_hidden)
@@ -119,14 +150,15 @@ class _MSAWeightedAveragingFused(torch.autograd.Function):
             no_heads,
             triton.cdiv(n_res, BLOCK_SIZE_ROW),
         )
-
+        
         MSAFwdFused[grid](
-            v, b, g, out, 
+            v, b, g, out,
             C_hidden, no_heads, inf,
             c_hidden_pow2, n_res_pow2, 
             n_seq, n_res,
             BLOCK_SIZE_ROW,
             BLOCK_SIZE_SEQ,
+            BLOCK_SIZE_COL,
         )
         
         return out
